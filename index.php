4ad2cd657895fe330f791bca4a11c824e8b1edc4
unknown <dheeraj0639@gmail.com>
2023-04-19 13:55:53 +0530

    Last audit

--- index.php
+++ index.php
@@ -35,13 +35,13 @@
             </div>
           </div>
           <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
+            <img src="https://cdn.pixabay.com/photo/2016/09/06/13/37/maserati-gran-turismo-1649119__340.jpg" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>Some representative placeholder content for the third slide.</p>
             </div>
           </div>
-        </div>
+          </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
@@ -77,13 +77,13 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Vintage G-88</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Car-Collection</p>
             </a>
-            <h6 class="mb-3">$61.99</h6>
+            <h6 class="mb-3">12,00,000</h6>
           </div>
         </div>
       </div>
@@ -106,13 +106,13 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Mustang</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop Now</p>
             </a>
-            <h6 class="mb-3">$61.99</h6>
+            <h6 class="mb-3">10,00,000</h6>
           </div>
         </div>
       </div>
@@ -134,14 +134,14 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">OG 6223</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop</p>
             </a>
             <h6 class="mb-3">
-              <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+              <s>25,00,000</s><strong class="ms-2 text-danger">20,00,000</strong>
             </h6>
           </div>
         </div>
@@ -170,13 +170,13 @@
           </div>
           <div class="card-body">
             <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+              <h5 class="card-title mb-3">KIA Mini</h5>
             </a>
             <a href="" class="text-reset">
-              <p>Category</p>
+              <p>Shop Now</p>
             </a>
             <h6 class="mb-3">
-              <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+              <s>25,00,000</s><strong class="ms-2 text-danger">20,99,999</strong>
             </h6>
           </div>
         </div>
@@ -198,13 +198,13 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Open Wintage CAr</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop Now</p>
             </a>
-            <h6 class="mb-3">$61.99</h6>
+            <h6 class="mb-3">25,00,000</h6>
           </div>
         </div>
       </div>
@@ -229,14 +229,14 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Volswegan</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop Now</p>
             </a>
             <h6 class="mb-3">
-              <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+              <s>6,00,000</s><strong class="ms-2 text-danger">5,00,000</strong>
             </h6>
           </div>
         </div>
@@ -263,6 +263,7 @@
 </div>
 
 </section >
+
 <section style="background-color: #eee;">
   <div class="text-center container py-5">
     <h4 class="mt-4 mb-5"><strong>Bestsellers</strong></h4>
@@ -272,7 +273,7 @@
         <div class="card">
           <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
             data-mdb-ripple-color="light">
-            <img src="https://cdn.pixabay.com/photo/2016/02/13/13/11/oldtimer-1197800_960_720.jpg"
+            <img src=" https://cdn.pixabay.com/photo/2016/03/27/19/44/car-1283947__340.jpg"
               class="w-100" />
             <a href="#!">
               <div class="mask">
@@ -286,13 +287,13 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Vintage G-88</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Car-Collection</p>
             </a>
-            <h6 class="mb-3">$61.99</h6>
+            <h6 class="mb-3">12,00,000</h6>
           </div>
         </div>
       </div>
@@ -301,7 +302,7 @@
         <div class="card">
           <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
             data-mdb-ripple-color="light">
-            <img src="https://cdn.pixabay.com/photo/2016/04/13/21/58/auto-1327801_960_720.jpg"
+            <img src="https://cdn.pixabay.com/photo/2016/11/22/23/55/car-1851299__340.jpg"
               class="w-100" />
             <a href="#!">
               <div class="mask">
@@ -315,13 +316,13 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Mustang</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop Now</p>
             </a>
-            <h6 class="mb-3">$61.99</h6>
+            <h6 class="mb-3">10,00,000</h6>
           </div>
         </div>
       </div>
@@ -329,7 +330,7 @@
       <div class="col-lg-4 col-md-6 mb-4">
         <div class="card">
           <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
-            <img src="https://cdn.pixabay.com/photo/2020/10/18/16/45/porsche-5665390_960_720.jpg"
+            <img src="https://cdn.pixabay.com/photo/2020/05/19/10/05/opel-5190050__340.jpg"
               class="w-100" />
             <a href="#!">
               <div class="mask">
@@ -343,14 +344,14 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">OG 6223</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop</p>
             </a>
             <h6 class="mb-3">
-              <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+              <s>25,00,000</s><strong class="ms-2 text-danger">20,00,000</strong>
             </h6>
           </div>
         </div>
@@ -361,7 +362,7 @@
       <div class="col-lg-4 col-md-12 mb-4">
         <div class="card">
           <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
-            <img src="https://cdn.pixabay.com/photo/2016/02/22/20/22/bmw-1216469__340.jpg"
+            <img src="https://cdn.pixabay.com/photo/2019/09/01/12/59/car-4445171__340.jpg"
               class="w-100" />
             <a href="#!">
               <div class="mask">
@@ -379,13 +380,13 @@
           </div>
           <div class="card-body">
             <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+              <h5 class="card-title mb-3">KIA Mini</h5>
             </a>
             <a href="" class="text-reset">
-              <p>Category</p>
+              <p>Shop Now</p>
             </a>
             <h6 class="mb-3">
-              <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+              <s>25,00,000</s><strong class="ms-2 text-danger">20,99,999</strong>
             </h6>
           </div>
         </div>
@@ -395,7 +396,7 @@
         <div class="card">
           <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
             data-mdb-ripple-color="light">
-            <img src="https://cdn.pixabay.com/photo/2019/08/08/23/33/car-4393990__340.jpg"
+            <img src="https://cdn.pixabay.com/photo/2012/11/03/22/41/ford-64041__340.jpg"
               class="w-100" />
             <a href="#!">
               <div class="mask">
@@ -407,13 +408,13 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Open Wintage CAr</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop Now</p>
             </a>
-            <h6 class="mb-3">$61.99</h6>
+            <h6 class="mb-3">25,00,000</h6>
           </div>
         </div>
       </div>
@@ -421,7 +422,7 @@
       <div class="col-lg-4 col-md-6 mb-4">
         <div class="card">
           <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
-            <img src="https://cdn.pixabay.com/photo/2017/03/27/14/56/auto-2179220_960_720.jpg"
+            <img src="https://cdn.pixabay.com/photo/2012/02/27/17/08/sportscar-17583__340.jpg"
               class="w-100" />
             <a href="#!">
               <div class="mask">
@@ -438,14 +439,14 @@
             </a>
           </div>
           <div class="card-body">
-            <a href="" class="text-reset">
-              <h5 class="card-title mb-3">Product name</h5>
+            <a href="shop1.php" class="text-reset">
+              <h5 class="card-title mb-3">Volswegan</h5>
             </a>
-            <a href="" class="text-reset">
-              <p>Category</p>
+            <a href="shop1.php" class="text-reset">
+              <p>Shop Now</p>
             </a>
             <h6 class="mb-3">
-              <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+              <s>6,00,000</s><strong class="ms-2 text-danger">5,00,000</strong>
             </h6>
           </div>
         </div>
@@ -456,130 +457,9 @@
 
 
 
-  <section>
-  <!-- Footer -->
- <footer class="text-center text-lg-start bg-light text-muted">
-  <!-- Section: Social media -->
-  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    <!-- Left -->
-    <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <!-- Left -->
-
-    <!-- Right -->
-    <div>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
-    </div>
-    <!-- Right -->
-  </section>
-  <!-- Section: Social media -->
-
-  <!-- Section: Links  -->
-  <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <!-- Grid row -->
-      <div class="row mt-3">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
-          </h6>
-          <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
-          </p>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Angular</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">React</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Vue</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Laravel</a>
-          </p>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Pricing</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Orders</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Help</a>
-          </p>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-          <p>
-            <i class="fas fa-envelope me-3"></i>
-            info@example.com
-          </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
-        </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
-    </div>
-  </section>
-  <!-- Section: Links  -->
-
-  <!-- Copyright -->
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2021 Copyright:
-    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-</footer>
-<!-- Footer --> 
-</section>
+<?php
+include('footer.php');
+?>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
     
